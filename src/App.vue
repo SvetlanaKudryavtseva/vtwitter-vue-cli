f66e2a8061579e5b798f0b33c0fb63e61a7b85e8
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2022-06-09 10:39:32 +0300

    creation of TweetsList component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <p>123</p>
+<div class="wrapper">
+  <!-- header -->
+  <div class="wrapper-content">
+    <div class="container">
+      <TweetsList />
+    </div>
+  </div><!-- end wrapper-content -->
+  <!-- footer -->
+</div><!-- end wrapper -->
+
 </template>
+<script>
+import TweetsList from '@/components/TweetsList'
+
+export default {
+  components: {TweetsList},
+}
+</script>
