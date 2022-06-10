3f6ebb29fd661434e883efa9438ca0b2b6a5f863
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2022-06-10 12:46:07 +0300

    start composition api - setup method

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <div class="wrapper-content">
     <div class="view">
       <div class="container">
-        <Form @onSubmit="handleSubmit"/>
+        <!-- <Form @onSubmit="handleSubmit"/> -->
         <TweetsList :items="items" />
       </div><!-- end container -->
     </div>
@@ -19,31 +19,54 @@ import TweetsList from '@/components/TweetsList';
 
 export default {
   components: { Form, TweetsList },
-  data() {
-        return {
-            items: [{
-                    id: 1,
-                    avatar: `https://avatars.dicebear.com/api/male/1.svg`,
-                    body: "hello vue 3",
-                    likes: 12,
-                    date: new Date(Date.now()).toLocaleString(),
-                },
-                {
-                    id: 2,
-                    avatar: `https://avatars.dicebear.com/api/male/2.svg`,
-                    body: "hello world",
-                    likes: 5,
-                    date: new Date(Date.now()).toLocaleString(),
-                },
-            ],
-        }
-    },
-    methods: {
-        handleSubmit(item){
-          this.items.push(item)
-        }
-
+  setup(){
+    const items = [
+      {
+          id: 1,
+          avatar: `https://avatars.dicebear.com/api/male/1.svg`,
+          body: "hello vue 3",
+          likes: 12,
+          date: new Date(Date.now()).toLocaleString(),
+      },
+      {
+          id: 2,
+          avatar: `https://avatars.dicebear.com/api/male/2.svg`,
+          body: "hello world",
+          likes: 5,
+          date: new Date(Date.now()).toLocaleString(),
+      },
+    ]
+    return {
+      items
     }
+  }
+// export default {
+//   components: { Form, TweetsList },
+//   data() {
+//         return {
+//             items: [{
+//                     id: 1,
+//                     avatar: `https://avatars.dicebear.com/api/male/1.svg`,
+//                     body: "hello vue 3",
+//                     likes: 12,
+//                     date: new Date(Date.now()).toLocaleString(),
+//                 },
+//                 {
+//                     id: 2,
+//                     avatar: `https://avatars.dicebear.com/api/male/2.svg`,
+//                     body: "hello world",
+//                     likes: 5,
+//                     date: new Date(Date.now()).toLocaleString(),
+//                 },
+//             ],
+//         }
+//     },
+//     methods: {
+//         handleSubmit(item){
+//           this.items.push(item)
+//         }
+
+//     }
     
 }
 </script>
