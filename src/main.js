8b7e33e9561d02add8f7a9279e71df027f8029e8
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2022-06-12 20:38:13 +0300

    add link to this course tutorials on youtube

--- src/main.js
+++ src/main.js
@@ -3,3 +3,5 @@ import App from './App.vue'
 
 import './assets/scss/main.scss'
 createApp(App).mount('#app')
+
+// курс по vue3 от tocode - https://www.youtube.com/watch?v=e6Q3rifOLxc&list=PLkCrmfIT6LBRcU37KZnMqy5hcTuwRhQkb
