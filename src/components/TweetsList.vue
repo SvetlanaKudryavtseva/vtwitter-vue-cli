aa05d0e7a23ed8446335ec720d9664bf86e6f7e5
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2022-06-12 14:40:56 +0300

    creation of select for sorting by date and likes

--- src/components/TweetsList.vue
+++ src/components/TweetsList.vue
@@ -1,10 +1,17 @@
 <template>
+    <select v-model="sortBy">
+        <option value="date">Sort by date</option>
+        <option value="likes">Sort by likes</option>
+    </select>
+
+    <p>{{ sortBy }}</p>
     <ul>
         <TweetsItem v-for="item in items" :key="item.id" :item="item"/>
     </ul>
 </template>
 
 <script>
+import { ref}  from 'vue'
 import TweetsItem from '@/components/TweetsItem.vue'
 
 export default {
@@ -14,6 +21,10 @@ export default {
             type: Array,
             required: true,
         }
+    },
+    setup() {
+        const sortBy = ref('date')
+        return { sortBy }
     }
 }
 </script>
